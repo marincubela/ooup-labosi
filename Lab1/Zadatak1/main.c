283874a1d2561fe60d767ac8271f2afc38325759
Marin Ćubela <mc51493@fer.hr>
2021-03-18 18:23:29 +0100

    Finish first task

---
+++ Lab1/Zadatak1/main.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char const* (*PTRFUN)();
+
+PTRFUN* dogVtable = NULL;
+PTRFUN* catVtable = NULL;
+
+char const* dogGreet(void) {
+    return "vau!";
+}
+
+char const* dogMenu(void) {
+    return "kuhanu govedinu";
+}
+
+char const* catGreet(void) {
+    return "mijau!";
+}
+
+char const* catMenu(void) {
+    return "konzerviranu tunjevinu";
+}
+
+void initializeDogVtable() {
+    dogVtable = (PTRFUN*)malloc(2 * sizeof(PTRFUN));
+    dogVtable[0] = dogGreet;
+    dogVtable[1] = dogMenu;
+}
+
+void initializeCatVtable() {
+    catVtable = (PTRFUN*)malloc(2 * sizeof(PTRFUN));
+    catVtable[0] = catGreet;
+    catVtable[1] = catMenu;
+}
+
+void initialize() {
+    initializeDogVtable();
+    initializeCatVtable();
+}
+
+struct Animal {
+    char const* ime;
+    PTRFUN* vptr;
+};
+
+void animalPrintGreeting(struct Animal* p) {
+    printf("%s\n", p->vptr[0]());
+}
+
+void animalPrintMenu(struct Animal* p) {
+    printf("%s\n", p->vptr[1]());
+}
+
+void constructDog(struct Animal* p, char const* ime) {
+    p->ime = ime;
+    p->vptr = dogVtable;
+}
+
+struct Animal* createDog(char const* ime) {
+    struct Animal* p = (struct Animal*)malloc(sizeof(struct Animal));
+    constructDog(p, ime);
+    return p;
+}
+
+void constructCat(struct Animal* p, char const* ime) {
+    p->ime = ime;
+    p->vptr = dogVtable;
+}
+
+struct Animal* createCat(char const* ime) {
+    struct Animal* p = (struct Animal*)malloc(sizeof(struct Animal));
+    constructCat(p, ime);
+    return p;
+}
+
+void testAnimals(void) {
+    struct Animal* p1 = createDog("Hamlet");
+    struct Animal* p2 = createCat("Ofelija");
+    struct Animal* p3 = createDog("Polonije");
+
+    animalPrintGreeting(p1);
+    animalPrintGreeting(p2);
+    animalPrintGreeting(p3);
+
+    animalPrintMenu(p1);
+    animalPrintMenu(p2);
+    animalPrintMenu(p3);
+
+    free(p1);
+    free(p2);
+    free(p3);
+}
+
+int main(void) {
+    initialize();
+    testAnimals();
+    return 0;
+}
