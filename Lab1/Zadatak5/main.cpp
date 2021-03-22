f67645e6d06013b6972ddd6cd8dde78d7cde83b3
Marin Ćubela <mc51493@fer.hr>
2021-03-22 19:57:40 +0100

    Finish task 5

--- Lab1/Zadatak5/main.cpp
+++ Lab1/Zadatak5/main.cpp
@@ -12,11 +12,20 @@ class D : public B {
     virtual int __cdecl druga(int x) { return prva() + x; }
 };
 
-typedef void (*PTRFUN)();
+typedef int (*fun1)(B *);
+typedef int (*fun2)(B *, int);
 
 void funkcija(B *pb) {
+    fun1 *vt = *(fun1 **)pb;
+
+    std::cout << "Povratna vrijednost prve funkcije: " << vt[0](pb) << std::endl;
+
+    fun2 *vt2 = *(fun2 **)pb;
+    std::cout << "Povratna vrijednost prve funkcije: " << vt2[1](pb, 1) << std::endl;
 }
 
 int main(void) {
+    B *pb = new D();
+    funkcija(pb);
     return 0;
 }
