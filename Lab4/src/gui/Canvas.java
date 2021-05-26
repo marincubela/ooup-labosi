a4999b5d7f6a6297a61c07c3733ac160a700202a
Marin Ćubela <mc51493@fer.hr>
2021-05-26 18:34:03 +0200

    Add state to gui

--- Lab4/src/gui/Canvas.java
+++ Lab4/src/gui/Canvas.java
@@ -1,25 +1,67 @@
 package gui;
 
-import graphics.Oval;
 import model.DocumentModel;
 import renderer.G2DRendererImpl;
 import renderer.Renderer;
-import utils.Rectangle;
+import state.IdleState;
+import utils.Point;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Canvas extends JComponent {
-    private DocumentModel model;
+    private final DocumentModel model;
+    private final GUI gui;
 
-    public Canvas(DocumentModel model) {
+    public Canvas(DocumentModel model, GUI gui) {
         this.model = model;
+        this.gui = gui;
+        setListeners();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         Renderer r = new G2DRendererImpl(g2d);
-        model.list().forEach(o -> o.render(r));
+        model.list().forEach(o -> {
+            o.render(r);
+            gui.getCurrentState().afterDraw(r, o);
+        });
+        gui.getCurrentState().afterDraw(r);
+    }
+
+
+    private void setListeners() {
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    gui.setCurrentState(new IdleState());
+                } else {
+                    gui.getCurrentState().keyPressed(e.getKeyCode());
+                }
+            }
+        });
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                gui.getCurrentState().mouseDown(new utils.Point(e.getX(), e.getY()), e.isShiftDown(), e.isControlDown());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                gui.getCurrentState().mouseUp(new utils.Point(e.getX(), e.getY()), e.isShiftDown(), e.isControlDown());
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                gui.getCurrentState().mouseDragged(new Point(e.getX(), e.getY()));
+            }
+        });
     }
 }
--- Lab4/src/gui/GUI.java
+++ Lab4/src/gui/GUI.java
@@ -2,6 +2,8 @@ package gui;
 
 import graphics.GraphicalObject;
 import model.DocumentModel;
+import state.IdleState;
+import state.State;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,12 +13,14 @@ public class GUI extends JFrame {
     private final List<GraphicalObject> objects;
     private final DocumentModel model;
     private final Canvas canvas;
+    private State currentState;
 
     public GUI(List<GraphicalObject> objects) {
         this.objects = objects;
         model = new DocumentModel();
+        currentState = new IdleState();
+        canvas = new Canvas(model, this);
 
-        canvas = new Canvas(model);
         setLocation(1000, 10);
         setSize(600, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -32,14 +36,20 @@ public class GUI extends JFrame {
         createToolBar(this.objects);
     }
 
-
     private void createToolBar(List<GraphicalObject> objects) {
         JToolBar toolBar = new JToolBar("Tools");
 
-        objects.forEach(o -> {
-            toolBar.add(new JButton(o.getShapeName()));
-        });
+        objects.forEach(o -> toolBar.add(new JButton(o.getShapeName())));
 
         this.getContentPane().add(toolBar, BorderLayout.PAGE_START);
+    }
+
+    public State getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(State state) {
+        currentState.onLeaving();
+        currentState = state;
     }
 }
