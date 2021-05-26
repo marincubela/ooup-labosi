5fc8d2320e012397b1ee68c6229e8f6a7e494eb1
Marin Ćubela <mc51493@fer.hr>
2021-05-26 12:14:50 +0200

    Add AbstractGraphicalObject

---
+++ Lab4/src/graphicals/AbstractGraphicalObject.java
@@ -0,0 +1,117 @@
+package graphicals;
+
+import renderer.Renderer;
+import utils.GeometryUtil;
+import utils.Point;
+import utils.Rectangle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public abstract class AbstractGraphicalObject implements GraphicalObject {
+    private Point[] hotPoints;
+    private boolean[] hotPointSelected;
+    private boolean selected;
+    private List<GraphicalObjectListener> listeners = new ArrayList<>();
+
+    public AbstractGraphicalObject(Point[] hotPoints) {
+        this.hotPoints = hotPoints;
+        hotPointSelected = new boolean[hotPoints.length];
+        Arrays.fill(hotPointSelected, false);
+    }
+
+    @Override
+    public boolean isSelected() {
+        return selected;
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+        notifySelectionListeners();
+    }
+
+    @Override
+    public int getNumberOfHotPoints() {
+        return hotPoints.length;
+    }
+
+    @Override
+    public Point getHotPoint(int index) {
+        return hotPoints[index];
+    }
+
+    @Override
+    public void setHotPoint(int index, Point point) {
+        hotPoints[index] = point;
+        notifyListeners();
+    }
+
+    @Override
+    public boolean isHotPointSelected(int index) {
+        return hotPointSelected[index];
+    }
+
+    @Override
+    public void setHotPointSelected(int index, boolean selected) {
+        hotPointSelected[index] = selected;
+        notifySelectionListeners();
+    }
+
+    @Override
+    public double getHotPointDistance(int index, Point mousePoint) {
+        return GeometryUtil.distanceFromPoint(hotPoints[index], mousePoint);
+    }
+
+    @Override
+    public void translate(Point delta) {
+        for (int i = 0; i < getNumberOfHotPoints(); i++) {
+            setHotPoint(i, hotPoints[i].translate(delta));
+        }
+        notifyListeners();
+    }
+
+    @Override
+    public void render(Renderer r) {
+        // TODO implement later or delete from here
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addGraphicalObjectListener(GraphicalObjectListener l) {
+        listeners.add(l);
+    }
+
+    @Override
+    public void removeGraphicalObjectListener(GraphicalObjectListener l) {
+        listeners.remove(l);
+    }
+
+    @Override
+    public String getShapeID() {
+        // TODO implement later or delete from here
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void load(Stack<GraphicalObject> stack, String data) {
+        // TODO implement later or delete from here
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void save(List<String> rows) {
+        // TODO implement later or delete from here
+        throw new UnsupportedOperationException();
+    }
+
+    public void notifyListeners() {
+        listeners.forEach(l -> l.graphicalObjectChanged(this));
+    }
+
+    public void notifySelectionListeners() {
+        listeners.forEach(l -> l.graphicalObjectSelectionChanged(this));
+    }
+}
