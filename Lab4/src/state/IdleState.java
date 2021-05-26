c93cdad970987f5e306b01f672ff170ca71005c8
Marin Ćubela <mc51493@fer.hr>
2021-05-26 18:33:44 +0200

    Add State and IdleState

---
+++ Lab4/src/state/IdleState.java
@@ -0,0 +1,42 @@
+package state;
+
+import graphics.GraphicalObject;
+import renderer.Renderer;
+import utils.Point;
+
+public class IdleState implements State{
+    @Override
+    public void mouseDown(Point mousePoint, boolean shiftDown, boolean ctrlDown) {
+        // Do nothing
+    }
+
+    @Override
+    public void mouseUp(Point mousePoint, boolean shiftDown, boolean ctrlDown) {
+        // Do nothing
+    }
+
+    @Override
+    public void mouseDragged(Point mousePoint) {
+        // Do nothing
+    }
+
+    @Override
+    public void keyPressed(int keyCode) {
+        // Do nothing
+    }
+
+    @Override
+    public void afterDraw(Renderer r, GraphicalObject go) {
+        // Do nothing
+    }
+
+    @Override
+    public void afterDraw(Renderer r) {
+        // Do nothing
+    }
+
+    @Override
+    public void onLeaving() {
+        // Do nothing
+    }
+}
---
+++ Lab4/src/state/State.java
@@ -0,0 +1,63 @@
+package state;
+
+import graphics.GraphicalObject;
+import renderer.Renderer;
+import utils.Point;
+
+public interface State {
+    /**
+     * Called when left mouse button is pressed
+     *
+     * @param mousePoint point at which was button pressed
+     * @param shiftDown  {@code true} if ctrl was down during click, else {@code false}
+     * @param ctrlDown   {@code true} if shift was down during click, else {@code false}
+     */
+    void mouseDown(Point mousePoint, boolean shiftDown, boolean ctrlDown);
+
+    /**
+     * Called when left mouse button is released
+     *
+     * @param mousePoint point at which was button pressed
+     * @param shiftDown  {@code true} if ctrl was down during click, else {@code false}
+     * @param ctrlDown   {@code true} if shift was down during click, else {@code false}
+     */
+    void mouseUp(Point mousePoint, boolean shiftDown, boolean ctrlDown);
+
+    /**
+     * Called when user drags mouse while key is pressed
+     *
+     * @param mousePoint point at which was button pressed
+     */
+    void mouseDragged(Point mousePoint);
+
+    /**
+     * Called when key press was registered
+     *
+     * @param keyCode pressed key
+     */
+    void keyPressed(int keyCode);
+
+    // Poziva se nakon što je platno nacrtalo grafički objekt predan kao argument
+
+    /**
+     * Called after canvas drew graphical object given as argument
+     *
+     * @param r renderer responsible for drawing objects
+     * @param go object to be drawn
+     */
+    void afterDraw(Renderer r, GraphicalObject go);
+
+    // Poziva se nakon što je platno nacrtalo čitav crtež
+
+    /**
+     * Called after canvas drew any object
+     *
+     * @param r renderer responsible for drawing objects
+     */
+    void afterDraw(Renderer r);
+
+    /**
+     * Called when program leaves this state
+     */
+    void onLeaving();
+}
